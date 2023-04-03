93df78aac6db4ef06e5bd6bf6dc995314e53d671
Precious <preciousmwangi34@gmail.com>
2023-04-03 10:16:12 +0300

    Authentication app

---
+++ app/src/main/java/com/example/authentication_android_pwm/Register_activity.kt
@@ -0,0 +1,11 @@
+package com.example.authentication_android_pwm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
